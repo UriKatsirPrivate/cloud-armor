081060258e3c87c8351ae42545d89cf5b92f1d2f
UriKatsirPrivate <46541339+UriKatsirPrivate@users.noreply.github.com>
2020-03-07 18:11:32 +0200

    Works with global variables

--- security_policies/main.py
+++ security_policies/main.py
@@ -1,17 +1,23 @@
 import count_rules
 import security_policies
 
+PROJECT_NAME = 'uri-test'
+POLICY_NAME = 'armor-policy2'
+
 
 def main():
 
-    all_policies = security_policies.get_all_policies()
+    all_policies = security_policies.get_all_policies(PROJECT_NAME)
 
     # rules_count = count_rules.count_policy_rules(
     #     '/Users/ukatsir/projects/cloud-armor/supporting_files/all_policies.json')
 
     rules_count = count_rules.count_policy_rules(all_policies)
 
-    fff = ''
+    one_policy = security_policies.get_one_policy(
+        PROJECT_NAME, POLICY_NAME)
+
+    fff = rules_count
 
 
 main()
--- security_policies/security_policies.py
+++ security_policies/security_policies.py
@@ -2,23 +2,33 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 import json
 
+flow = InstalledAppFlow.from_client_secrets_file(
+    'supporting_files/client_secret_960394617171.json',
+    scopes=['https://www.googleapis.com/auth/cloud-platform'])
 
-def get_all_policies():
+credentials = flow.run_local_server()
+# credentials = flow.run_console()
 
-    flow = InstalledAppFlow.from_client_secrets_file(
-        'supporting_files/client_secret_960394617171.json',
-        scopes=['https://www.googleapis.com/auth/cloud-platform'])
+security_policy_service = build('compute', 'v1', credentials=credentials)
 
-    credentials = flow.run_local_server()
-    # credentials = flow.run_console()
 
-    security_policy_service = build('compute', 'v1', credentials=credentials)
+def get_all_policies(project_name):
 
     policies = security_policy_service.securityPolicies().list(
-        project='uri-test').execute()
+        project=project_name).execute()
 
     all_policies = json.dumps(policies)
 
-    # id = policies['items'][0]['id']
+    id = policies['items'][0]['id']
 
     return all_policies
+
+
+def get_one_policy(project_name, policy_name):
+    policies = security_policy_service.securityPolicies().get(
+        project=project_name, securityPolicy=policy_name).execute()
+
+    one_policy = json.dumps(policies)
+
+    name = policies['name']
+    return one_policy
