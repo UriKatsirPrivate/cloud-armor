05e0e763800cc3d365b6412664c9c3b3ff69f6b3
UriKatsirPrivate <46541339+UriKatsirPrivate@users.noreply.github.com>
2020-03-10 10:14:19 +0200

    good

--- lists.py
+++ lists.py
@@ -9,8 +9,8 @@ import operator
 
 from jsonpath_ng import jsonpath, parse
 
-def create_lists():
 
+def create_lists():
 
     listoflists = []
     allow_preview_list = []
@@ -39,34 +39,35 @@ def create_lists():
             allow_preview_list.append(rule.value)
             current_rule_index = len(allow_preview_list)
             allow_preview_list[(current_rule_index-1)
-                            ]['number_of_ips'] = number_of_ips
+                               ]['number_of_ips'] = number_of_ips
         elif action == 'allow' and preview == 'False':
             allow_no_preview_list.append(rule.value)
             current_rule_index = len(allow_no_preview_list)
             allow_no_preview_list[(current_rule_index-1)
-                                ]['number_of_ips'] = number_of_ips
+                                  ]['number_of_ips'] = number_of_ips
         elif 'deny' in action and preview == 'True':
             deny_preview_list.append(rule.value)
             current_rule_index = len(deny_preview_list)
             deny_preview_list[(current_rule_index-1)
-                            ]['number_of_ips'] = number_of_ips
+                              ]['number_of_ips'] = number_of_ips
 
         elif 'deny' in action and preview == 'False':
             deny_no_preview_list.append(rule.value)
             current_rule_index = len(deny_no_preview_list)
             deny_no_preview_list[(current_rule_index-1)
-                                ]['number_of_ips'] = number_of_ips
-
+                                 ]['number_of_ips'] = number_of_ips
 
     # sorted_allow_no_preview_list = sorted(
     #     allow_no_preview_list, key='number_of_ips')
 
     stop_here = ''
-    combine_ones(allow_no_preview_list)
+    combine_rules(allow_no_preview_list,1)
 
 
-def combine_ones(input_list):
+def combine_rules(input_list,number_of_ips_to_match):
     json_list = json.dumps(input_list)
-    print (json_list)
+    # print (json_list)
+    
+
 
 create_lists()
